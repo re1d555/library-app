3eac13e71b5782ac8723f1a17de74a7d46e4d254
re1d555 <55630923+re1d555@users.noreply.github.com>
2024-04-12 21:20:16 +0300

    Add dialog

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
 const cards = document.querySelector('.book-cards');
+const addBtn = document.querySelector('.add-btn');
+const closeBtn = document.querySelector('.modal-end');
+const dialog = document.querySelector('dialog');
+const titleInput = document.querySelector('.title-input');
+const authorInput = document.querySelector('.author-input');
+const pagesInput = document.querySelector('.pages-input');
+const confirmBtn = document.querySelector('.confirm-btn');
 
-const book1 = new Book('Before they hanged', 'Joe Abercrombie', 441, 'read');
-const book2 = new Book('The Wise Man`s Fear', 'Patrick Rothfuss', 994, 'not read');
-const book3 = new Book('Old Man`s War ', 'John Scalzi', 352, 'not read');
+// const book1 = new Book('Before they hanged', 'Joe Abercrombie', 441, 'read');
+// const book2 = new Book('The Wise Man`s Fear', 'Patrick Rothfuss', 994, 'not read');
+// const book3 = new Book('Old Man`s War ', 'John Scalzi', 352, 'not read');
 
-
-const myLibrary = [book1, book2, book3];
+const myLibrary = [];
 
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read;
+    // this.read = read;
+
+
+    // need indicator, if object already on screen: true
+    // the same indicator can use for delete concrete object from array
+
 }
 
 function addBookToLibrary() {
-
+    myLibrary.push(new Book(titleInput.value, authorInput.value, pagesInput.value));
+    booksOnDisplay();
 }
 
 function booksOnDisplay() {
@@ -43,8 +55,6 @@ function booksOnDisplay() {
     }
 }
 
-booksOnDisplay();
-
 // horizontal book cards scrolling
 window.addEventListener('wheel', (e) => {
     if (e.deltaY > 0) {
@@ -54,3 +64,16 @@ window.addEventListener('wheel', (e) => {
     }
 })
 
+addBtn.addEventListener('click', (e) => {
+    dialog.showModal();
+});
+
+closeBtn.addEventListener('click', (e) => {
+    dialog.close();
+});
+
+confirmBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    addBookToLibrary();
+    dialog.close();
+});
