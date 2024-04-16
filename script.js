a4effe8cc52e854f360750ba0b4063eccdf97624
re1d555 <55630923+re1d555@users.noreply.github.com>
2024-04-16 12:20:10 +0300

    Done

--- script.js
+++ script.js
@@ -7,24 +7,32 @@ const authorInput = document.querySelector('.author-input');
 const pagesInput = document.querySelector('.pages-input');
 const confirmBtn = document.querySelector('.confirm-btn');
 const dialogRead = document.querySelector('#read');
+const form = document.querySelector('form');
 
 // const book1 = new Book('Before they hanged', 'Joe Abercrombie', 441, 'read');
 // const book2 = new Book('The Wise Man`s Fear', 'Patrick Rothfuss', 994, 'not read');
-// const book3 = new Book('Old Man`s War ', 'John Scalzi', 352, 'not read');
+// const book3 = new Book('Old Man`s War', 'John Scalzi', 352, 'not read');
 
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages, read, onscreen) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
+    // this.onscreen = onscreen;
 }
 
 function addBookToLibrary() {
     const newBook = new Book(titleInput.value, authorInput.value, pagesInput.value, dialogRead.checked);
-    newBook.bookOnDisplay();
     myLibrary.push(newBook);
+    newBook.bookOnDisplay();
+
+    // for (i = 0; i < myLibrary.length; i++) {
+    //     if (!myLibrary[i].onscreen === true) {
+    //         myLibrary[i].bookOnDisplay();
+    //     }
+    // }
 }
 
 Book.prototype.bookOnDisplay = function() {
@@ -35,7 +43,7 @@ Book.prototype.bookOnDisplay = function() {
     const del = document.createElement('button');
     del.textContent = 'X';
     del.addEventListener('click', () => {
-        myLibrary.splice(this, 1);
+        myLibrary.splice(myLibrary.indexOf(this), 1);
         cards.removeChild(card);
     });
 
@@ -50,12 +58,27 @@ Book.prototype.bookOnDisplay = function() {
         
     const cardRead = document.createElement('div');
     this.read === true ? cardRead.textContent = 'read' : cardRead.textContent = 'not read yet';
-    
+
+    const readStatus = document.createElement('button');
+    readStatus.textContent = 'status update'
+    readStatus.addEventListener('click', () => {
+        if (this.read === true) {
+            this.read = false;
+            cardRead.textContent = 'not read yet';
+        } else {
+            this.read = true;
+            cardRead.textContent = 'read';
+        }
+    });
+
+    // this.onscreen = true;
+
     card.appendChild(del);
     card.appendChild(title);
     card.appendChild(author);
     card.appendChild(pages);
     card.appendChild(cardRead);
+    card.appendChild(readStatus);
 }
 
 // horizontal book cards scrolling
@@ -77,11 +100,14 @@ closeBtn.addEventListener('click', (e) => {
     cleanInputs()
 });
 
+// validation check before preventDefault
 confirmBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    addBookToLibrary();
-    dialog.close();
-    cleanInputs()
+    if (form.checkValidity() === true && form.reportValidity() === true) {
+        e.preventDefault();
+        addBookToLibrary();
+        cleanInputs()
+        dialog.close();
+    } 
 });
 
 dialog.addEventListener('cancel', cleanInputs);
